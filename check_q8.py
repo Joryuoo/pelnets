4a77780a32237b989402359565bba354f6e4e91b
John Kyle Villarazo <villarazojk@gmail.com>
2026-09-09 01:47:42 +0800

    fix: minor changes in %% ex. 2024S_FE-A_83 %% and 2021 October AM to be April AM

--- check_q8.py
+++
@@ -1,9 +0,0 @@
-import fitz
-
-pdf_path = r"C:\Users\Kyle\Downloads\pelnets\Exam_Q&A\2022\April\2022S_FE_PM_Question.pdf"
-doc = fitz.open(pdf_path)
-
-for i in range(len(doc)):
-    text = doc[i].get_text("text")
-    if "Q8" in text or "Question 8" in text:
-        print(f"Found Q8 on Page {i+1}")
